f44aec72893099d45fc4e40cf429309cd84b9396
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-24 15:24:21 +0530

    ADDED: Function to reset game board

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -2,3 +2,22 @@
 
 echo "/******************************************* WELCOME TO TIC TAC TOE GAME *******************************************/"
 
+# CONSTANTS
+NUMBER_OF_ROWS=3
+NUMBER_OF_COLUMNS=3
+
+# DECLARING DICTIONARY FOR GAME
+declare -A gameBoard
+
+# FUNCTION TO RESET GAME BOARD
+function resetGameBoard(){
+	for (( row=0; row<$NUMBER_OF_ROWS; row++ ))
+	do
+		for ((column=0; column<$NUMBER_OF_COLUMNS; column++ ))
+		do
+			gameBoard[$row,$column]=" "
+		done
+	done
+}
+
+resetGameBoard
